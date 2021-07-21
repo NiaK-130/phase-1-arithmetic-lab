2efc59779b9d755df381e82678b319d0bb47d849
Nia K <nsjkappen@gmail.com>
2021-07-21 15:07:41 -0700

    commit tree 5b732550840848601e845149bab773d241e341bc

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-
 // Write your code here
+
+var num1 = 2;
+var num2 = 31;
+
+const multiply = (num1*num2);
+
+console.log(multiply)
+
+// ------------------------------------------------//
+
+const random = Math.floor(Math.random() *10)+1;
+
+console.log(random)
+
+// ------------------------------------------------//
+
+var num3 = 20;
+var num4 = 5;
+const mod = (num3/num4);
+
+console.log(mod)
+
+// ------------------------------------------------//
+
+const max = Math.max(1, 2, 3, 4, 20);
+console.log(max)
